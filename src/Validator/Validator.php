a3ec4dca0c5cbb100b3d33d2813faf316e351666
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2021-05-21 14:32:25 +0000

    Commit missing interfaces declaration

---
+++ src/Validator/Validator.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drewlabs\Contracts\Validator;
+
+interface Validator extends ValidationErrorProvider
+{
+
+    /**
+     * Validate provided dirty input against a set of rules and messages
+     *
+     * @param Validatable|array|string $validatable
+     * @param array $input
+     * @param array|null $input
+     * @return static
+     */
+    public function validate($validatable, array $input, $messages = array());
+
+    /**
+     * Model {is_update} property setter
+     *
+     * @param boolean $update
+     * @return static
+     */
+    public function setUpdate(bool $update);
+}
