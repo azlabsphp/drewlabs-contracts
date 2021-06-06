18fa7cb8f1d06999e653b7f1121a1083e4b4f6ae
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2021-06-06 19:52:40 +0000

    Revert ModelRepository interface back to default implemtation

--- src/Data/Repository/ModelRepository.php
+++ src/Data/Repository/ModelRepository.php
@@ -2,8 +2,6 @@
 
 namespace Drewlabs\Contracts\Data\Repository;
 
-
-/** @package Drewlabs\Contracts\Data\Repository */
 interface ModelRepository
 {
 
@@ -25,38 +23,44 @@ interface ModelRepository
 
     /**
      * Add or insert new record to the data storage
+     * @param array $data
+     * @param bool $parse_inputs
+     * @param bool $upsert
+     * @param array $upsertConditions
+     * @return mixed
      */
-    public function insert(...$args);
+    public function insert(array $values, bool $parse_inputs = false, $upsert = false, $upsertConditions = array());
 
     /**
-     * Create many model entries in the data storage
-     *
-     * @param array ...$args
-     */
-    public function insertMany(...$args);
+    * Create many model entries in the data storage
+    *
+    * @param array $values
+    * @param boolean $parse_inputs
+    * @return mixed
+    */
+    public function insertMany(array $values, $parse_inputs = true);
 
     /**
      * Return a new model matching a certain condition
      *
-     * @param array ...$args
+     * @param array $conditions
+     * @param array $columns
+     * @return array|mixed
      */
-    public function find(...$args);
+    public function find(array $conditions = array(), array $columns = array('*'));
 
     /**
      * Return a new model with the given id
      *
-     * @deprecated v3.0.1 Use the {@see find()} overloaded method that takes in an id
-     * 
      * @param int $id
      * @param array $columns
      * @return mixed
      */
-    public function findById(int $id, array $columns = array('*'));
+    public function findById($id, array $columns = array('*'));
 
     /**
      * Update a given record in the data storage using model id
      *
-     * @deprecated v3.0.1 Use {@see update($id, array $attributes)} method overload instead
      * @param int $id
      * @param array $data
      * @param bool $parse_inputs
@@ -67,9 +71,12 @@ interface ModelRepository
     /**
      * Update a given record in the data storage based on condition
      *
-     * @param array ...$args
+     * @param array $data
+     * @param array $conditions
+     * @param bool $parse_inputs
+     * @return int
      */
-    public function update(...$args);
+    public function update(array $data, array $conditions = array(), bool $parse_inputs = true, bool $mass_update =  false);
 
     /**
      * Delete record from data storage based on model id
@@ -82,9 +89,11 @@ interface ModelRepository
     /**
      * Delete record from data storage based on condition
      *
-     * @param array ...$args
+     * @param array $conditions
+     * @param bool $mass_delete
+     * @return int
      */
-    public function delete(...$args);
+    public function delete(array $conditions = array(), bool $mass_delete =  false);
 
     /**
      * Get the current repository model class name
