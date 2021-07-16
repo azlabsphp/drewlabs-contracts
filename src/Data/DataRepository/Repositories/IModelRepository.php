198ed247845dfee7712a44a28b1ea5787e2f07ca
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2021-07-16 08:30:37 +0000

    Makes IModelRepository deprecated

--- src/Data/DataRepository/Repositories/IModelRepository.php
+++ src/Data/DataRepository/Repositories/IModelRepository.php
@@ -4,6 +4,9 @@ namespace Drewlabs\Contracts\Data\DataRepository\Repositories;
 
 use Drewlabs\Contracts\Data\Repository\ModelRepository;
 
+/**
+ * @deprecated 1.0
+ */
 interface IModelRepository extends ModelRepository
 {
 }
