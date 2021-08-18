bd15aa8a4837c5d7169ce753a0ca5f7d6f6e29e9
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2021-08-18 22:08:35 +0000

    Added the option \Closure parameter to ActionHandler::handle method

--- src/Support/Actions/ActionHandler.php
+++ src/Support/Actions/ActionHandler.php
@@ -2,14 +2,18 @@
 
 namespace Drewlabs\Contracts\Support\Actions;
 
+use Closure;
+
 interface ActionHandler
 {
     /**
-     * Handle a specific action and return a result
+     * Handle a specific action and return a result. The callback is provided for handling
+     * action after the action is performed
      * 
      * @param Action $action
+     * @param Closure $callback
      * 
      * @return ActionResult|mixed
      */
-    public function handle(Action $action);
+    public function handle(Action $action, Closure $callback = null);
 }
