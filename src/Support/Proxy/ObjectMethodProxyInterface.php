e12f20358b7c6bc284808b201c77abc88f109c38
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2021-08-19 15:07:51 +0000

    Added a proxy() method

--- src/Support/Proxy/ObjectMethodProxyInterface.php
+++ src/Support/Proxy/ObjectMethodProxyInterface.php
@@ -5,6 +5,18 @@ namespace Drewlabs\Contracts\Support\Proxy;
 interface ObjectMethodProxyInterface
 {
     /**
+     * {@link proxy()} method provide the same method definition and can be used as
+     * an alternative
+     * 
+     * @param \stdClass|object $name
+     * @param mixed $method 
+     * @param array $args 
+     * @param \Closure $default
+     * @return mixed
+     */
+    public function forwardCallTo($object, $method, $args = [], \Closure $default = null);
+
+    /**
      * Provide a dynamic method proxy interface to the provided object.
      * if the specified method does not exists, the default method is called
      * instead
@@ -15,5 +27,5 @@ interface ObjectMethodProxyInterface
      * @param \Closure $default
      * @return mixed
      */
-    public function forwardCallTo($object, $method, $args = [], \Closure $default = null);
+    public function proxy($object, $method, $args = [], \Closure $default = null);
 }
