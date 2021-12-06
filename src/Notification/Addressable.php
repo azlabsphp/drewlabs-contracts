a5baf99867c72ceb6041af7ea4e7e96f3970fd8b
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2021-12-06 20:54:23 +0000

    Add Addressable interface that can be returned by the INotification::getSender() and INotification::getReceiver() methods

---
+++ src/Notification/Addressable.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Drewlabs package.
+ *
+ * (c) Sidoine Azandrew <azandrewdevelopper@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Drewlabs\Contracts\Notification;
+
+interface Addressable
+{
+    /**
+     * Returns the identifier attached to the address object
+     * 
+     * @return string 
+     */
+    public function id();
+
+    /**
+     * Returns the name attached to the address object
+     * 
+     * @return string|null 
+     */
+    public function name();
+
+    /**
+     * Compute the string value of the address
+     * 
+     * @return string 
+     */
+    public function __toString();
+}
